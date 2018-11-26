f4d97de3fa275dedeadb34e753fb585dd1703c56
Charles Ellis <hamled@hamled.name>
2018-11-26 10:16:50 -0800

    Remove bins and game logic

--- src/App.js
+++ src/App.js
@@ -5,30 +5,12 @@ import Trash from './components/Trash.js';
 class App extends Component {
   constructor() {
     super();
+
     this.state = {
-      bins: this.getBinsState(),
-      points: 0
+      points: 0,
     };
 
-    this.startGame();
-    console.log(this.state.bins);
-  }
-
-  startGame() {
-    setInterval(() => {
-      this.setState( {
-        bins: this.getBinsState()
-      });
-    }, 1500);
-  }
-
-  getBinsState() {
-    let bins = [];
-    for (let i = 0; i < 9; i++){
-      bins.push({ isTrashVisible: (Math.round(Math.random()) ? true : false )});
-    }
-
-    return bins;
+    console.log(this.state);
   }
 
   onTrashClicked = () => {
@@ -36,21 +18,11 @@ class App extends Component {
   }
 
   render() {
-    const bins = this.state.bins.map((bin, index) => {
-      return (
-        <Trash key={`trash-${index}`} />
-      );
-    });
-
     return (
       <div className="App">
         <section className="overall-data">
           <h1>Litter Patrol</h1>
           <h2>Points: { this.state.points }</h2>
-        </section>
-
-        <section className="bins">
-          { bins }
         </section>
       </div>
     );
