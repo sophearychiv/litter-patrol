ba73f9be51d9a330be41fbdedf3d2aa155575d72
Charles Ellis <hamled@hamled.name>
2018-11-26 10:31:21 -0800

    Parallax level background
    This background is made up of about 10 different layers, each with their
    own PNG background image. The parallax effect is made by scrolling them
    each at a different rate.

--- src/App.js
+++ src/App.js
@@ -61,6 +61,7 @@ class App extends Component {
         </section>
 
         <section className="level">
+          { this.levelBackground() }
           { objects }
         </section>
 
@@ -139,6 +140,16 @@ class App extends Component {
 
   enableSpawner() {
     this.spawnObjects = true;
+  }
+
+  levelBackground() {
+    const layers = ['clouds-1', 'clouds-2', 'clouds-3', 'clouds-4',
+                    'hills-1','hills-2','bushes','trees-1','trees-2','ground'];
+    return (
+      <div className="level-bg">
+        {layers.map(layer => (<div className={`level-bg-${layer}`} key={layer} />))}
+      </div>
+    );
   }
 
   componentDidMount() {
