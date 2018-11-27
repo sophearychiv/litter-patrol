bc34c9616a7bc87cdd9479e6eb68e40238938b56
Charles Ellis <hamled@hamled.name>
2018-11-26 23:16:17 -0800

    Updates

---
+++ precache-manifest.4c1cbc9d7cd75bb66dac9d09022582ab.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "498f9992458c9effd1af",
+    "url": "/litter-patrol/static/css/main.b9c65b67.chunk.css"
+  },
+  {
+    "revision": "498f9992458c9effd1af",
+    "url": "/litter-patrol/static/js/main.498f9992.chunk.js"
+  },
+  {
+    "revision": "fb594e236ac8b4cb04bc",
+    "url": "/litter-patrol/static/js/1.fb594e23.chunk.js"
+  },
+  {
+    "revision": "2b5fd1116da1670d264c",
+    "url": "/litter-patrol/static/js/runtime~main.2b5fd111.js"
+  },
+  {
+    "revision": "83c4acd2d5e97821735846455fc38fcb",
+    "url": "/litter-patrol/static/media/background.83c4acd2.png"
+  },
+  {
+    "revision": "3c04d98c9f0fe334f1151d23f6a38265",
+    "url": "/litter-patrol/static/media/clouds-1.3c04d98c.png"
+  },
+  {
+    "revision": "5ccc31c7f71649d97b6f46a70a43799e",
+    "url": "/litter-patrol/static/media/clouds-2.5ccc31c7.png"
+  },
+  {
+    "revision": "7198032280e50a7d97a7506e268d5a9a",
+    "url": "/litter-patrol/static/media/clouds-3.71980322.png"
+  },
+  {
+    "revision": "65f7c026c185da0ef809b253e8244a74",
+    "url": "/litter-patrol/static/media/hills-1.65f7c026.png"
+  },
+  {
+    "revision": "161eb7a5fcb689c3f43fe33ded4d3cfc",
+    "url": "/litter-patrol/static/media/hills-2.161eb7a5.png"
+  },
+  {
+    "revision": "b88bdd80f2c7c94bf0da4f92b2f6ee11",
+    "url": "/litter-patrol/static/media/bushes.b88bdd80.png"
+  },
+  {
+    "revision": "ca68c20d5092fa895684d64b606e070d",
+    "url": "/litter-patrol/static/media/trees-1.ca68c20d.png"
+  },
+  {
+    "revision": "367537d31f348c274016605d95fe2fc3",
+    "url": "/litter-patrol/static/media/clouds-4.367537d3.png"
+  },
+  {
+    "revision": "baa0a430c166400b9bba756f2b1d6336",
+    "url": "/litter-patrol/static/media/trees-2.baa0a430.png"
+  },
+  {
+    "revision": "1540f58a6222f939bbf022bdeda2fa2b",
+    "url": "/litter-patrol/static/media/ground.1540f58a.png"
+  },
+  {
+    "revision": "1b3062697025e1f508a0f5fac0ea2c1a",
+    "url": "/litter-patrol/static/media/rock.1b306269.png"
+  },
+  {
+    "revision": "5e5a6c1ce662c2755d084918e0010a43",
+    "url": "/litter-patrol/static/media/bush.5e5a6c1c.png"
+  },
+  {
+    "revision": "89a9414aeb97c7cee4cfd56ce0ae90c9",
+    "url": "/litter-patrol/static/media/flower.89a9414a.png"
+  },
+  {
+    "revision": "092ee2df837666f28f7a3892d23c987c",
+    "url": "/litter-patrol/static/media/mushroom.092ee2df.png"
+  },
+  {
+    "revision": "68803946eb4fbe880a26dd4b2c6b742d",
+    "url": "/litter-patrol/static/media/logo.68803946.png"
+  },
+  {
+    "revision": "5e7579c719655f673c61ad7e65a22f5c",
+    "url": "/litter-patrol/index.html"
+  }
+];
